b58237db08803746f8c1a7badc0e2dc3b2e246b6
binarioGH <lacuentafalsadediegojonas@gmail.com>
2018-11-24 01:18:18 -0600

    Errores arreglados

---
+++ cfernet.py
@@ -0,0 +1,36 @@
+#-*-coding:utf-8-*-
+from cryptography.fernet import Fernet
+from sys import argv
+
+
+if __name__ == '__main__':
+	count = 0
+	clave = ""
+	texto = ""
+	crpt = True
+	for arg in argv:
+		if arg[0] != "-":
+			count+=1
+			continue
+		else:
+			if arg == "-key":
+				clave = argv[count + 1].encode()
+			elif arg == "-text":
+				texto = argv[count + 1].encode()
+			elif arg == "--decrypt":
+				crpt = False
+			count += 1
+	if clave == "":
+		print("[+] Generando clave...")
+		clave = Fernet.generate_key()
+		print(clave.decode())
+	f = Fernet(clave)
+
+	if crpt == True:
+		print("[+] Encriptando...")
+		token = f.encrypt(texto)
+		print(token.decode())
+	else:
+		print("[+] Desencriptando...")
+		des = f.decrypt(texto)
+		print(des.decode())
