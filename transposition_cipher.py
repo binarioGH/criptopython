b1b55460caa2331fdcaad81c97da74a6c36f85f7
binarioGH <lacuentafalsadediegojonas@gmail.com>
2019-01-11 21:28:41 -0600

    modo de decodificar

--- transposition_cipher.py
+++ transposition_cipher.py
@@ -2,16 +2,20 @@
 from lib import pyperclip
 from sys import argv
 from platform import python_version as pv
+from optparse import OptionParser as op
+from math import ceil
 def main():
-	try:
-		mykey = argv[2]
-	except IndexError:
-		mykey = 4
-
+	opt = op("Usage: %prog [args] [values]")
+	opt.add_option("-k", "--key", dest="key",help="Set key.",default=4,type="int")
+	opt.add_option("-d", "--do", dest="do", help="If this argument is equal to its default value ('encode') it will encode your text, else, it will decode it.",default="encode")
+	(o, argv) = opt.parse_args()
 	if pv()[0] == "3":
 		raw_input = input
 	mytext = raw_input("Input your text here: ")
-	encoded = encode(mytext, mykey)
+	if o.do == "encode":
+		encoded = encode(mytext, o.key)
+	else:
+		encoded = decode(mytext,o.key)
 	print(encoded)
 	pyperclip.copy(encoded)
 
@@ -23,7 +27,20 @@ def encode(text, key):
 			ciphertext[column] += text[currentindex]
 			currentindex += key
 	return ''.join(ciphertext)
-
+def decode(text, key):
+	numofcolumns = int(ceil(len(text)/float(key)))
+	numofrows = key
+	numofshadeboxes = (numofcolumns * numofrows) - len(text)
+	plaintext = [''] * numofcolumns 
+	row = 0
+	column = 0
+	for symbol in text:
+		plaintext[column] += symbol
+		column += 1
+		if (column == numofcolumns) or (column == numofcolumns -1 and row >= numofrows  - numofshadeboxes):
+			column = 0
+			row += 1
+	return ''.join(plaintext)
 
 if __name__ == '__main__':
 	main()
